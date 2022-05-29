6b23fd0cc89bc137a33bad54d8fd4a0f8c6e8397
42hena <somnium1108@gmail.com>
2022-05-30 00:24:40 +0900

    Refactor

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -1,21 +1,19 @@
-#include "Zombie.hpp"
 #include <iostream>
-Zombie::Zombie()
-{
-}
+
+#include "Zombie.hpp"
 
 Zombie::Zombie(std::string name)
 {
-    this->name = name;
-    std::cout << "<" << name << "> create..." << std::endl;
+	this->name = name;
+	std::cout << "<" << name << "> create..." << std::endl;
 }
 
 void Zombie::announce(void)
 {
-    std::cout << "<" << name << "> BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << "<" << name << "> BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
 Zombie::~Zombie()
 {
-    std::cout << "<" << name << "> died..." << std::endl;
+	std::cout << "<" << name << "> died..." << std::endl;
 }
--- cpp01/ex01/Zombie.hpp
+++ cpp01/ex01/Zombie.hpp
@@ -1,18 +1,23 @@
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
+
 #include <string>
+
 class Zombie
 {
 private:
-
 public:
     std::string name;
-    Zombie();
+    // 생성자
     Zombie(std::string name);
-
+    // 소멸자
     ~Zombie();
+    
+    // 맴버 함수
     void announce(void);
-
 };
-Zombie* zombieHorde(int N, std::string name);
+
+// 전역 함수
+Zombie *zombieHorde(int N, std::string name);
+
 #endif
