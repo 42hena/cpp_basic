f8d75f9e634853a034f9d621e573e5862829961b
42hena <somnium1108@gmail.com>
2022-05-30 00:29:33 +0900

    Refactor

--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -1,15 +1,20 @@
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
+
 #include <string>
+
 class Weapon
 {
 private:
 	std::string type;
-	
+
 public:
-	Weapon(const std::string& type);
+	// 생성자
+	Weapon(const std::string &type);
+	// 소멸자
 	~Weapon(void);
 
+	// 멤버 함수
 	const std::string&	getType(void) const;
 	void				setType(const std::string &type);
 };
