d0901ae0330a270025924b97b030ca136e5029f8
42hena <somnium1108@gmail.com>
2022-06-02 06:29:16 +0900

    Feat : cpp02 - ex01

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,59 +1,76 @@
-#include "Fixed.hpp"
 #include <iostream>
+
+#include "Fixed.hpp"
 
 const int Fixed::fractionalBit = 8;
 
+// default 생성자
 Fixed::Fixed(void)
 {
-    std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
     this->fixedPoint = 0;
 }
 
-Fixed::Fixed(const int i)
+Fixed::Fixed(const int integer)
 {
-    std::cout << "Int constructor called" << std::endl;
-    this->fixedPoint = 0;
+	std::cout << "Int constructor called" << std::endl;
+    this->fixedPoint = integer * (1 << this->fractionalBit);
 }
 
-Fixed::Fixed(const float f)
+Fixed::Fixed(const float floatPoint)
 {
-    std::cout << "Float constructor called" << std::endl;
-    this->fixedPoint = 0;
+	std::cout << "Float constructor called" << std::endl;
+    this->fixedPoint = floatPoint * (1 << this->fractionalBit);
 }
 
-
+// copy 생성자
 Fixed::Fixed(const Fixed &copy)
-    : fixedPoint(copy.fixedPoint)
+    // : fixedPoint(copy.fixedPoint)
 {
-    // this->fixedPoint = copy.fixedPoint;
     std::cout << "Copy constructor called" << std::endl;
-    *this = copy;
+	*this = copy;
 }
 
+// 복사 대입 생성자
 Fixed& Fixed::operator=(const Fixed &copy)
-{   
-    // fractionalBit = copy.fractionalBit;
-//    this->fractionalBit = copy.fractionalBit;
-   std::cout << "Copy assignment operator called" << std::endl; 
+{	
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->fixedPoint = copy.getRawBits();
 
-    fixedPoint = copy.getRawBits();
     return *this;
 }
 
-int Fixed::getRawBits(void) const
-{
-    // std::cout << "getRawBits member function called" << std::endl;
-    return this->fixedPoint;
-}
-
+// 소멸자
 Fixed::~Fixed(void)
 {
     std::cout << "Destructor called" << std::endl;
 }
 
-// std::ostream& operator<<(std::ostream& outputStream, const Fixed& A)
-// {
-//     std::cout << "(" << A.() << ", " << A.getY() << ")";
-//     return outputStream;
-// }
+// getter setter
+int Fixed::getRawBits(void) const
+{
+	return this->fixedPoint;
+}
 
+void	Fixed::setRawBits(int const raw)
+{
+	this->fixedPoint = raw;
+}
+
+// 멤버 함수
+float Fixed::toFloat(void) const
+{
+    return  ((float)this->getRawBits()) / (1 << this->fractionalBit);
+}
+
+int Fixed::toInt(void) const
+{
+    return  this->getRawBits() / (1 << this->fractionalBit);
+}
+
+// << 오버로딩
+std::ostream& operator<<(std::ostream& outputStream, const Fixed& A)
+{
+    std::cout << (float)A.getRawBits() / (1 << 8);
+    return outputStream;
+}
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -1,17 +1,35 @@
-#include <iostream>
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
 class Fixed
 {
 private:
-	int	fixedPoint;
-	static const int fractionalBit;
+	int 				fixedPoint;
+	static const int	fractionalBit;
+
 public:
+	// 생성자
 	Fixed(void);
-	Fixed(const int i);
-	Fixed(const float f);
+	Fixed(const int);
+	Fixed(const float);
+
+	// 복사 생성자
 	Fixed(const Fixed &copy);
+
+	// 복사 대입 연산자
 	Fixed& operator=(const Fixed &copy);
+
+	// 소멸자
 	~Fixed(void);
-	int	getRawBits(void) const;
-	void	setRawBits(int const raw);
+
+	// getter, setter
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	
+	float toFloat(void) const;
+	int	toInt(void) const;
 };
+#include <iostream>
 std::ostream& operator<<(std::ostream& outputStream, const Fixed& A);
+
+#endif
