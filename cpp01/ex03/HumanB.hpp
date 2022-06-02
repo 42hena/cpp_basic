fe1df95a2d278f4f01e1d192764c3103c7106cfc
42hena <somnium1108@gmail.com>
2022-06-03 01:41:19 +0900

    Refactor :

--- cpp01/ex03/HumanB.hpp
+++ cpp01/ex03/HumanB.hpp
@@ -1,16 +1,23 @@
 #ifndef HUMANB_HPP
 #define HUMANB_HPP
+
 #include "Weapon.hpp"
+
 class HumanB
 {
 private:
-    Weapon      *weapon;
-    std::string name;
+	Weapon      *weapon;
+	std::string name;
 public:
-    HumanB(std::string name);
-    ~HumanB();
-    void setWeapon(Weapon &weapon);
-    void attack(void);
+	// 생성자
+	HumanB(std::string name);
+
+	// 소멸자
+	~HumanB();
+
+	// 멤버 함수
+	void	setWeapon(Weapon &weapon);
+	void	attack(void);
 };
 
 #endif
