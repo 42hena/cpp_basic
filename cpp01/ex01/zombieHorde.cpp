0850b888236d57a06045b14b27d7e04dbf41f159
42hena <somnium1108@gmail.com>
2022-06-02 23:16:36 +0900

    Refactor : remove lines

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -5,9 +5,10 @@ Zombie* zombieHorde(int N, std::string name)
     Zombie* horde = new Zombie[N];
     
     for (int i = 0 ; i < N ; ++i)
+    {
         horde[i].setName(name);
-    for (int i = 0 ; i < N ; ++i)
         horde[i].announce();
-        
+    }
+ 
     return (horde);
 }
