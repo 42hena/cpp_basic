c5e771b170a267c97e63c215a7c5b62f7d4867e5
42hena <somnium1108@gmail.com>
2022-06-02 23:14:07 +0900

    Add : setName

--- cpp01/ex01/Zombie.hpp
+++ cpp01/ex01/Zombie.hpp
@@ -6,18 +6,23 @@
 class Zombie
 {
 private:
-public:
     std::string name;
+public:
+    
     // 생성자
+    Zombie(void);
     Zombie(std::string name);
     // 소멸자
     ~Zombie();
     
+    //getter setter
+    void    setName(std::string &name);
+
     // 맴버 함수
-    void announce(void);
+    void    announce(void);
 };
 
 // 전역 함수
-Zombie *zombieHorde(int N, std::string name);
+Zombie*     zombieHorde(int N, std::string name);
 
 #endif
