fd58b28e80927723e96c00bb70493b17af18ae3b
42hena <somnium1108@gmail.com>
2022-06-02 23:14:46 +0900

    important : about "pop_back"
    - pop_back function is c++ 11... so cant use it.

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -1,18 +1,13 @@
 #include "Zombie.hpp"
+
 Zombie* zombieHorde(int N, std::string name)
 {
     Zombie* horde = new Zombie[N];
-    std::string value;
     
     for (int i = 0 ; i < N ; ++i)
-    {
-        horde[i].name = name;
-        char number = i + '0';
-        horde[i].name.push_back(number);
-    }
+        horde[i].setName(name);
     for (int i = 0 ; i < N ; ++i)
-    {
         horde[i].announce();
-    }
+        
     return (horde);
 }
