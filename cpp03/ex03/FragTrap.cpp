7259b8b117b3979333a9f37e73f176271602f95f
42hena <somnium1108@gmail.com>
2022-06-06 23:05:09 +0900

    Feat : virtual

---
+++ cpp03/ex03/FragTrap.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include "FragTrap.hpp"
+
+// 기본 생성자
+FragTrap::FragTrap(void)
+	: ClapTrap("NoName")
+{
+	hitPoint = 100;
+	energyPoint = 100;
+	attackDamage = 30;
+	std::cout << "FragTrap Class "<< name << " Default Constructor" << std::endl;
+}
+
+// 생성자
+FragTrap::FragTrap(std::string name)
+	: ClapTrap(name)
+{
+	hitPoint = 100;
+	energyPoint = 100;
+	attackDamage = 30;
+	std::cout << "FragTrap Class "<< name << " Constructor" << std::endl;
+}
+
+// 소멸자
+FragTrap::~FragTrap()
+{
+	std::cout << "FragTrap Class " << name << " Default Destructor" << std::endl;    
+}
+
+// 복사 생성자
+FragTrap::FragTrap(const FragTrap& copy)
+{
+	*this = copy;
+}
+
+// 복사 대입 연산자
+FragTrap &FragTrap::operator=(FragTrap const &copy)
+{
+	this->name = copy.getName();
+	return *this;
+}
+
+// 멤버 함수
+void FragTrap::highFivesGuys(void)
+{
+	if (!hitPoint)
+		std::cout << "FragTrap " << name << " asks for a positive high-five" << std::endl;
+	else
+		std::cout << "FragTrap " << name << " can't ask for a positive high-five because " << name << " is dead" << std::endl;
+}
---
+++ cpp03/ex03/FragTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : virtual public ClapTrap
+{
+public:
+	// 생성자
+	FragTrap();
+	FragTrap(std::string name);
+
+	// 소멸자
+	~FragTrap();
+
+	// 복사 생성자
+	FragTrap(const FragTrap& copy);
+
+	// 복사 대입 연산자
+	FragTrap &operator=(FragTrap const &copy);
+
+	// 멤버 함수
+	void highFivesGuys(void);
+};
+
+#endif
