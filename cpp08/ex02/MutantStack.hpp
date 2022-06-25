fc8f95cbd7dea480cbddd162e03310fafa57e147
42hena <somnium1108@gmail.com>
2022-06-25 22:05:34 +0900

    Feat : delete c++ 11 function

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -9,12 +9,12 @@ class MutantStack : public std::stack<T>
 public:
 	typedef typename std::stack<T>::container_type::iterator
 		iterator;
-	typedef typename std::stack<T>::container_type::const_iterator
-		const_iterator;
+	// typedef typename std::stack<T>::container_type::const_iterator
+	// 	const_iterator;
 	typedef typename std::stack<T>::container_type::reverse_iterator
 		reverse_iterator;
-	typedef typename std::stack<T>::container_type::const_reverse_iterator
-		const_reverse_iterator;
+	// typedef typename std::stack<T>::container_type::const_reverse_iterator
+	// 	const_reverse_iterator;
 
 	iterator begin(void)
 	{
@@ -26,16 +26,6 @@ public:
 		return this->c.end();
 	}
 
-	const_iterator cbegin(void) const
-	{
-		return this->c.cbegin();
-	}
-
-	const_iterator cend(void) const
-	{
-		return this->c.cend();
-	}
-
 	reverse_iterator rbegin(void)
 	{
 		return this->c.rbegin();
@@ -44,16 +34,6 @@ public:
 	reverse_iterator rend(void)
 	{
 		return this->c.rend();
-	}
-
-	const_reverse_iterator crbegin(void) const
-	{
-		return this->c.crbegin();
-	}
-
-	const_reverse_iterator crend(void) const
-	{
-		return this->c.crend();
 	}
 	
 	// 생성자
